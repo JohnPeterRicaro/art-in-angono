1e114865bfe96fc894e930cf1df7061176d43c63
JohnPeterRicaro <johnpeterricaro@gmail.com>
2024-11-06 04:41:23 +0800

    removed unnecssary timer stuff

--- src/app/tracking/timer/page.tsx
+++ src/app/tracking/timer/page.tsx
@@ -17,10 +17,7 @@ const Page = () => {
       Notification.requestPermission();
     }
 
-    const initialTourTime =
-      (museumsInRoute[0]?.tour_eta?.tour_time || 0) -
-      (museumsInRoute[0]?.tour_eta?.tour_time || 0) +
-      0.1;
+    const initialTourTime = museumsInRoute[0]?.tour_eta?.tour_time || 0;
     if (initialTourTime) {
       setTimeLeft(initialTourTime * 60);
     }
